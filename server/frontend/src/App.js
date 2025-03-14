460b1c3028bda65c549e79c72d4e5233a1d149f2
SiwyInf <marcel2003@spoko.pl>
2025-03-14 09:04:28 -0400

    dealres

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import Register from './components/Register/Register'; // jeśli komponent znajduje się w folderze components
 import { Routes, Route } from "react-router-dom";
-
+import Dealers from './components/Dealers/Dealers';
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
+            <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
